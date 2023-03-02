77abdedd1295b8781ef0d7cc83774f3e223ddba0
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2023-03-02 12:51:45 -0800

    refactor: feeding data into custom AprilTagDetection struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use apriltag::{Detection, DetectorBuilder, TagParams, Image, Family};
-/**
-.
+use apriltag::{Detection, DetectorBuilder, Family, Image, MatdRef, TagParams, Pose, families::ApriltagFamily};
 
-# Panics
+// Delete before releasing, meant for debugging / printing
+#[derive(Debug)]
+struct AprilTagDetection {
+    id: usize,
+    confidence: f32,
+    error: f32,
+    pose: Option<Pose>
+}
 
-Panics if .
-*/
 fn main() {
     let path = concat!(env!("CARGO_MANIFEST_DIR"), "/test_data/apriltag_board.pnm");
     let image = Image::from_pnm_file(path).unwrap();
@@ -15,23 +18,24 @@ fn main() {
         .build()
         .expect("Valid builder");
 
-    let detections: Vec<Detection> = detector.detect(&image);   
+    let detections: Vec<Detection> = detector.detect(&image);
 
-    // detections.into_iter().enumerate().for_each(|(index, det)| {
-    //     println!("  - detection {index}: {det:#?}");
-    // });
-
-    let tag_params = TagParams { //! idk these numbers
-        cx : 0.0,
-        cy : 0.0,
-        fx : 220.0,
-        fy : 220.0,
-        tagsize : 16.0,
+    let tag_params = TagParams {
+        cx: 0.0,
+        cy: 0.0,
+        fx: 220.0,
+        fy: 220.0,
+        tagsize: 16.0,
     };
     
-    detections.into_iter().enumerate().for_each(|(index, det)| {
-        let fin_pose = det.estimate_tag_pose(&tag_params);
-        println!(" - detection {index}: {det:#?}");
-        println!(" - pose {index}: {fin_pose:#?}");
-    });
+
+    detections
+        .into_iter()
+        .enumerate()
+        .for_each(|(index, detection)| {
+            let estimated_pose = detection.estimate_tag_pose(&tag_params);
+            let apriltag_detection = AprilTagDetection { id: detection.id(), confidence: 10.3, error: 10.3, pose: estimated_pose };
+
+            println!("apriltag_detection {index} {:#?}", apriltag_detection.id);
+        });
 }
