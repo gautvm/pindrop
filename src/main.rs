92706e58c7996615cdcd4c1d51f6c4f4b8f0b321
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2023-03-02 14:10:21 -0800

    refactor: refactored code to use unwrap_or_else for better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,25 @@
-use apriltag::{
-    Detection, DetectorBuilder, Family, Image, Pose, TagParams,
-};
+use apriltag::{Detection, DetectorBuilder, Family, Image, Pose, TagParams};
 
-// Delete before releasing, meant for debugging / printing
+// Delete before releasing, meant for enabling debugging / printing
 #[derive(Debug)]
 struct AprilTagDetection {
     id: usize,
     confidence: f32,
     error: f32,
-    pose: Option<Pose>,
+    pose: Pose,
 }
 
 fn main() {
     let path = concat!(env!("CARGO_MANIFEST_DIR"), "/test_data/apriltag_board.pnm");
-    let image = Image::from_pnm_file(path).unwrap();
+    let image = match Image::from_pnm_file(path) {
+        Ok(image) => image,
+        Err(e) => panic!("Error loading image: {}", e),
+    };
 
     let mut detector = DetectorBuilder::new()
         .add_family_bits(Family::tag_16h5(), 1)
         .build()
-        .expect("Valid builder");
+        .unwrap_or_else(|e| panic!("Error building detector: {}", e));
 
     let detections: Vec<Detection> = detector.detect(&image);
 
@@ -34,7 +35,8 @@ fn main() {
         .into_iter()
         .enumerate()
         .for_each(|(index, detection)| {
-            let estimated_pose = detection.estimate_tag_pose(&tag_params);
+            let estimated_pose = detection.estimate_tag_pose(&tag_params)
+    .unwrap_or_else(|| panic!("Failed to estimate pose for detection {}", detection.id()));
             let apriltag_detection = AprilTagDetection {
                 id: detection.id(),
                 confidence: 10.3,
@@ -42,6 +44,6 @@ fn main() {
                 pose: estimated_pose,
             };
 
-            println!("apriltag_detection {index} {:#?}", apriltag_detection.id);
+            println!("apriltag_detection {}: {{ id: {}, confidence: {}, error: {}, pose: {:#?} }}", index, apriltag_detection.id, apriltag_detection.confidence, apriltag_detection.error, apriltag_detection.pose)
         });
 }
