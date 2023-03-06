db39f5dfddb893d20382094534b6f23e45147a07
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2023-03-06 10:54:59 -0800

    feat: added script to deploy on raspberry pi

---
+++ scripts/deploy/rpi.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Raspberry Pi IP address and username
+RASPBERRY_PI_IP_ADDRESS="koh-raspi.local"
+RASPBERRY_PI_HOSTNAME="pi"
+
+# Path to code source directory
+SRC_DIRECTORY="./"
+
+# Name of the binary
+BINARY_NAME="pindrop"
+
+# Path to the binary
+RASPBERRY_PI_BINARY_PATH="/home/pi/$BINARY_NAME"
+
+# Code build command
+BUILD_SOURCE_COMMAND="cargo build --release"
+
+# SCP local files to the pi 
+SCP_COMMAND="scp $SRC_DIRECTORY/target/release/$BINARY_NAME $RASPBERRY_PI_HOSTNAME@$RASPBERRY_PI_IP_ADDRESS:$RASPBERRY_PI_BINARY_PATH"
+
+# SSH command to execute binary
+SSH_COMMAND="ssh $RASPBERRY_PI_USERNAME@$RASPBERRY_PI_IP_ADDRESS $RASPBERRY_PI_BINARY_PATH"
+
+# Build source code
+echo "Building Pindrop binary..."
+cd $SRC_DIRECTORY
+$BUILD_SOURCE_COMMAND
+
+# Deploy and scp code
+echo "Copying Pindrop"
+$SCP_COMMAND
+
+# Run binary
+echo "Executing Pindrop"
+$SSH_COMMAND
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod cli;
+pub mod field;
 pub mod geometry;
 pub mod pose;
 pub mod pose_estimator;
-pub mod field;
