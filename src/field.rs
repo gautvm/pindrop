72103935731810284d821e900ba916783e8f366c
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2023-03-05 23:57:25 -0800

    refactor: field constants

---
+++ src/field.rs
@@ -0,0 +1,104 @@
+use nalgebra::{Isometry3, Quaternion, Translation3, UnitQuaternion};
+use std::collections::HashMap;
+
+pub enum Game {
+    ChargedUp,
+}
+
+pub struct Field {
+    april_tags: HashMap<i32, Isometry3<f64>>,
+    length: f64,
+    width: f64,
+}
+
+impl Field {
+    pub fn new(game: Game) -> Self {
+        let mut april_tags = HashMap::new();
+        match game {
+            Game::ChargedUp => {
+                april_tags.insert(
+                    1,
+                    Isometry3::from_parts(
+                        Translation3::new(15.513558, 1.071626, 0.462788),
+                        UnitQuaternion::from_quaternion(Quaternion::new(0.0, 0.0, 0.0, 1.0)),
+                    ),
+                );
+                april_tags.insert(
+                    2,
+                    Isometry3::from_parts(
+                        Translation3::new(15.513558, 2.748026, 0.462788),
+                        UnitQuaternion::from_quaternion(Quaternion::new(0.0, 0.0, 0.0, 1.0)),
+                    ),
+                );
+                april_tags.insert(
+                    3,
+                    Isometry3::from_parts(
+                        Translation3::new(15.513558, 4.424426, 0.462788),
+                        UnitQuaternion::from_quaternion(Quaternion::new(0.0, 0.0, 0.0, 1.0)),
+                    ),
+                );
+                april_tags.insert(
+                    4,
+                    Isometry3::from_parts(
+                        Translation3::new(16.178784, 6.749796, 0.695452),
+                        UnitQuaternion::from_quaternion(Quaternion::new(0.0, 0.0, 0.0, 1.0)),
+                    ),
+                );
+                april_tags.insert(
+                    5,
+                    Isometry3::from_parts(
+                        Translation3::new(0.36195, 6.749796, 0.695452),
+                        UnitQuaternion::from_quaternion(Quaternion::new(1.0, 0.0, 0.0, 0.0)),
+                    ),
+                );
+                april_tags.insert(
+                    6,
+                    Isometry3::from_parts(
+                        Translation3::new(1.02743, 4.424426, 0.462788),
+                        UnitQuaternion::from_quaternion(Quaternion::new(1.0, 0.0, 0.0, 0.0)),
+                    ),
+                );
+                april_tags.insert(
+                    7,
+                    Isometry3::from_parts(
+                        Translation3::new(1.02743, 2.748026, 0.462788),
+                        UnitQuaternion::from_quaternion(Quaternion::new(1.0, 0.0, 0.0, 0.0)),
+                    ),
+                );
+                april_tags.insert(
+                    8,
+                    Isometry3::from_parts(
+                        Translation3::new(1.02743, 1.071626, 0.462788),
+                        UnitQuaternion::from_quaternion(Quaternion::new(1.0, 0.0, 0.0, 0.0)),
+                    ),
+                );
+            }
+        }
+
+        let (length, width) = match game {
+            Game::ChargedUp => (16.54175, 8.0137),
+        };
+
+        Self {
+            april_tags,
+            length,
+            width,
+        }
+    }
+
+    pub fn get_april_tags(&self) -> &HashMap<i32, Isometry3<f64>> {
+        &self.april_tags
+    }
+
+    pub fn get_april_tag_by_id(&self, id: i32) -> Option<&Isometry3<f64>> {
+        self.april_tags.get(&id)
+    }
+
+    pub fn get_width(&self) -> f64 {
+        self.width
+    }
+
+    pub fn get_length(&self) -> f64 {
+        self.length
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod cli;
 pub mod geometry;
 pub mod pose;
 pub mod pose_estimator;
+pub mod field;
