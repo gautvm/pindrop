9844e12676ecb9fb8549fe4165b12ea9260fcfc0
= <=>
2023-05-15 03:35:37 +0100

    updated cargo without rscam and updates to v4l imp

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,77 @@
-use rscam;
-use std::fs;
-use std::io::Write;
-use std::time::{Duration, Instant};
+use std::{io, fmt::format};
+use std::time::Instant;
 
-const FRAME_COUNT: usize = 100;
+use v4l::buffer::Type;
+use v4l::io::traits::CaptureStream;
+use v4l::prelude::*;
+use v4l::video::Capture;
 
-fn main() {
-    let mut camera = rscam::new("/dev/video0").unwrap();
+//use image::{DynamicImage, GenericImageView, ImageBuffer, Luma, GrayImage};
+use apriltag::{Detector, Family, DetectorBuilder, Detection, Image};
+use apriltag_image::{ImageExt, image::ImageBuffer};
 
-    camera
-        .start(&rscam::Config {
-            interval: (1, 30), // 30 fps.
-            resolution: (1280, 720),
-            //format: b"MJPG",
-            ..Default::default()
-        })
-        .unwrap();
 
-    let mut i = 0;
-    let mut fps_sum = Duration::from_secs(0);
-    let mut start_time = Instant::now();
-    loop {
-        let frame = camera.capture().unwrap();
-        //let mut file = fs::File::create(&format!("frame-{}.jpg", i)).unwrap();
-        //file.write_all(&frame[..]).unwrap();
+fn main() -> io::Result<()> {
+    let path = "/dev/video0";
+    println!("Using device: {}\n", path);
 
-        let elapsed_time = start_time.elapsed();
-        fps_sum += elapsed_time;
-        if i % FRAME_COUNT == 0 {
-            let fps = (FRAME_COUNT as f64 / fps_sum.as_secs_f64()).round();
-            println!("FPS: {}", fps);
-            fps_sum = Duration::from_secs(0);
-        }
-        start_time = Instant::now();
+    // Capture 4 frames by default
+    let count = 4;
 
-        i += 1;
+    // Allocate 4 buffers by default
+    let buffer_count = 4;
+
+    let dev = Device::with_path(path)?;
+    let format = dev.format()?;
+    //let params: v4l::video::capture::Parameters = dev.params()?;
+    let mut params = dev.params()?;
+    params.interval.denominator = 30;
+    params.interval.numerator = 1;
+    dev.set_params(&params)?;
+
+    println!("Active format:\n{}", format);
+    println!("Active parameters:\n{}", params);
+
+    // Setup a buffer stream and grab a frame, then print its data
+    let mut stream = MmapStream::with_buffers(&dev, Type::VideoCapture, buffer_count)?;
+
+    // warmup
+    stream.next()?;
+    
+    let mut detector = DetectorBuilder::new()
+    .add_family_bits(Family::tag_16h5(), 1)
+    .build()
+    .unwrap_or_else(|e| panic!("Error building detector: {}", e));
+
+    let start = Instant::now();
+    let mut megabytes_ps: f64 = 0.0;
+    for i in 0..count {
+        let t0 = Instant::now();
+        let (buf, meta) = stream.next()?;
+        let duration_us = t0.elapsed().as_micros();
+
+        let img_buff = ImageBuffer::from_raw(1280, 720, &buf[..]).unwrap();
+        let img = Image::from_image_buffer(&img_buff);
+        let result = img_buff.save_with_format("yeah.png",apriltag_image::image::ImageFormat::Png );
+        // Detect AprilTags in the image
+        
+        let detections: Vec<Detection> = detector.detect(&img);
+        println!("{:#?}", detections);
+        detections.into_iter().enumerate().for_each(|(index, det)| {
+            println!("  - detection {index}: {det:#?}");
+        });
+
+
+        println!("Buffer");
+        println!("  sequence  : {}", meta.sequence);
+        println!("  timestamp : {}", meta.timestamp);
+        println!("  flags     : {}", meta.flags);
+        println!("  length    : {}", buf.len());
     }
+
+    println!();
+    println!("FPS: {}", count as f64 / start.elapsed().as_secs_f64());
+    println!("MB/s: {}", megabytes_ps);
+
+    Ok(())
 }
